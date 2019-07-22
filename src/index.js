4a0e81daa234843d966d6c40fb177b1fe133f917
emizzle <eric.mastro@gmail.com>
2019-07-22 14:56:46 +1000

    Account management to support Embark blockchain config
    - manually exclude the test account in —sirius as it cannot be unlocked
    - support `nodeAccounts` config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const DEV_ACCOUNT = {
   address: "0xc916cfe5c83dd4fc3c3b0bf2ec2d4e401782875e",
   password: "WelcomeToSirius"
 };
+const TEST_ACCOUNT = {
+  address: "0xd119b8b038d3a67d34ca1d46e1898881626a082b"
+};
 
 export default class EmbarkLightchain {
   constructor(embark) {
@@ -19,8 +22,6 @@ export default class EmbarkLightchain {
     // gets hydrated blockchain config from embark, use it to init
     this.events.once("config:load:blockchain", (blockchainConfig) => {
       this.blockchainConfig = blockchainConfig;
-
-      this.embark.logger.info("======> loaded blockchain config");
     });
     this.events.once("blockchain:ready", () => {
       this.onBlockchainReady();
@@ -30,78 +31,91 @@ export default class EmbarkLightchain {
 
   onBlockchainReady() {
     this.events.request("blockchain:get", async (web3) => {
-      // if we're in dev mode, unlock our developer account
-      if (this.isDev) {
-        const {address, password} = DEV_ACCOUNT;
-        await web3.eth.personal.unlockAccount(address, password);
-        this.embark.logger.info(`Unlocked lightchain dev account '${address}'`);
-      }
+      this.web3 = web3;
       try {
-        await this.createAccounts(web3);
+        const configuredNodeAccounts = this.blockchainConfig.accounts && this.blockchainConfig.accounts.find(account => account.nodeAccounts);
+        let password = "";
+        if (configuredNodeAccounts) {
+          if (configuredNodeAccounts.password) {
+            try {
+              password = fs.readFileSync(configuredNodeAccounts.password).toString();
+            } catch (err) {
+              throw new Error(`Error reading password file '${configuredNodeAccounts.password}': ${err.message || err}`)
+            }
+          }
+          await this.ensureNodeAccounts(configuredNodeAccounts, password);
+          await this.unlockNodeAccounts(password);
+        }
       } catch (err) {
         this.embark.logger.error(err.message);
       }
     });
   }
 
-  async createAccounts(web3) {
-    const nodeAccount = this.blockchainConfig.accounts && this.blockchainConfig.accounts.find(account => account.nodeAccounts);
-    if (!nodeAccount) return;
+  get nodeAccounts() {
+    return (async () => {
+      const accounts = await this.web3.eth.personal.getAccounts();
+      return accounts.filter((account) =>
+        // remove the test account that is meant for internal use only
+        account.toLowerCase() !== TEST_ACCOUNT.address.toLowerCase()
+      );
+    })();
+  }
 
-    if (this.isDev) {
-      this.embark.logger.warn("'nodeAccounts' is specified in the development blockchain config, but lightchain includes it's own node account for development, so this configuration option will be ignored.");
-    }
+  async ensureNodeAccounts(configuredNodeAccounts, password) {
+    if (!configuredNodeAccounts) return;
 
-    if (nodeAccount.numAccounts) {
-      this.embark.logger.warn("'numAccounts' was specified in the blockchain config but it will be ignored. This plugin only supports one node account.");
-    }
-
-
-    let password = "";
-    if (nodeAccount.password) {
-      try {
-        password = fs.readFileSync(nodeAccount.password).toString();
-      } catch (err) {
-        throw new Error(`Error reading password file '${nodeAccount.password}': ${err.message || err}`)
-      }
-    }
+    const ensureNumNodeAccounts = parseInt(configuredNodeAccounts.numAccounts || 1, 10);
 
     // create node accounts if needed
-    const currNodeAccounts = await web3.eth.personal.getAccounts();
-    let address = "";
-    if (!currNodeAccounts.length) {
+    let accountsCreated = 0;
+    const currNodeAccounts = await this.nodeAccounts;
+    if (currNodeAccounts.length < ensureNumNodeAccounts) {
       if (password === "") {
         throw new Error("Cannot create a node account with a blank password. Please specify a password in the blockchain config.");
       }
       try {
-        address = await web3.eth.personal.newAccount(password);
-        this.embark.logger.info(`Created node account '${address}'`);
+        while ((currNodeAccounts.length + accountsCreated++) < ensureNumNodeAccounts) {
+          const address = await this.web3.eth.personal.newAccount(password);
+          this.embark.logger.info(`Created node account '${address}'`);
+        }
       } catch (err) {
-        throw new Error(`Error creating a new node account: ${err.message || err}`);
+        throw new Error(`Error creating new node account: ${err.message || err}`);
       }
-    } else {
-      address = currNodeAccounts[0];
     }
+  }
+
+  async unlockNodeAccounts(password) {
+    const currNodeAccounts = await this.nodeAccounts;
 
     // unlock node account - warn if no password specified
     if (password === "") {
       this.embark.logger.warn("A password was not specified for the node account. A blank password will be used to unlock the account.");
     }
-    try {
-      const unlockResult = await web3.eth.personal.unlockAccount(address, password);
-      if (unlockResult) {
-        this.embark.logger.info(`Successfully unlocked node account '${address}'`);
-      } else {
-        throw new Error(`Could not unlock node account '${address}'`);
+
+    const errors = [];
+    for (let address of currNodeAccounts) {
+      try {
+        let unlockPassword = password;
+        let isDeveloperAcct = false;
+        if (this.isDev && address.toLowerCase() === DEV_ACCOUNT.address.toLowerCase()) {
+          unlockPassword = DEV_ACCOUNT.password;
+          isDeveloperAcct = true;
+        }
+        const unlockResult = await this.web3.eth.personal.unlockAccount(address, unlockPassword);
+        if (unlockResult) {
+          this.embark.logger.info(`Successfully unlocked ${isDeveloperAcct ? "developer" : ""} node account '${address}'`);
+        } else {
+          errors.push(new Error(`Account unlock failed for '${address}'`));
+        }
+      }
+      catch (err) {
+        errors.push(new Error(`Could not unlock node account '${address}': ${err.message || err}`));
       }
     }
-    catch (err) {
-      throw new Error(`Could not unlock node account '${address}': ${err.message || err}`);
+    if (errors.length) {
+      throw new Error(`Error unlocking node accounts: ${errors.join("\n")}`);
     }
-
-
-    // nodeAccounts is not supported. For more information, please read https://notes.status.im/4Zvs5EcUR_-Eu-BOwnCNEw
-    //this.embark.logger.warn("Specifying blockchain config node accounts is not supported using a blockchain plugin yet. You can still specify wallet accounts in the config or create your own node accounts manually.");
   }
 
   registerBlockchain() {
